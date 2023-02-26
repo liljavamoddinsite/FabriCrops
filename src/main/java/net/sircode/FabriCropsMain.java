ae9086362caad69d1f779a653f3827f30085cc26
Sir Code <taylorschneider07@yahoo.com>
2023-02-25 20:28:35 -0500

    Add initial setup + some assets

---
+++ src/main/java/net/sircode/FabriCropsMain.java
@@ -0,0 +1,12 @@
+package net.sircode;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabriCropsMain implements ModInitializer {
+	public static final String MODID = "fabricrops";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
